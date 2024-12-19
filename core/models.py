8b58e4d2a4a8a188083db52f2732a4ae6588b2d1
Vitor Toledo <vitor.toledo@grupomateus.com>
2024-12-18 21:47:13 -0300

    models suggestions

--- core/models.py
+++ core/models.py
@@ -1,6 +1,18 @@
+from django.db import models
 from utils.models import SoftDeleteModel
 
 
 # Create your models here.
 class Project(SoftDeleteModel):
     ...
+
+
+class CompanyAddress(SoftDeleteModel):
+    ...
+
+
+class Company(SoftDeleteModel):
+    name = models.CharField(verbose_name="Name of the company")
+    # Mudar para https://pypi.org/project/django-cpf-cnpj/
+    # cnpj = models.CharField(verbose_name="Name of the company")
+    address = models.ForeignKey(CompanyAddress, on_delete=models.CASCADE)
