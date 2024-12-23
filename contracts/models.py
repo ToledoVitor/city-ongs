884ee6fccce910ee6fa62cd7ee9faf7ec73831af
Vitor Toledo <vitor.toledo@grupomateus.com>
2024-12-23 13:02:58 -0300

    first migrations

--- contracts/models.py
+++ contracts/models.py
@@ -6,7 +6,9 @@ from utils.choices import StatusChoices
 class Contract(models.Model):
     name = models.CharField(verbose_name="Nome do contrato", max_length=128)
     total_value = models.DecimalField(
-        verbose_name="Valor do contrato", decimal_places=2
+        verbose_name="Valor do contrato",
+        decimal_places=2,
+        max_digits=12,
     )
     start_of_vigency = models.DateField(verbose_name="Começo da vigência")
     end_of_vigency = models.DateField(verbose_name="Fim da vigência")
@@ -22,7 +24,10 @@ class ContractGoal(models.Model):
     name = models.CharField(verbose_name="Meta", max_length=128)
     description = models.CharField(verbose_name="Descrição", max_length=128)
     status = models.CharField(
-        verbose_name="Status", choices=StatusChoices, default=StatusChoices.DRAFT
+        verbose_name="Status",
+        max_length=16,
+        choices=StatusChoices,
+        default=StatusChoices.DRAFT,
     )
 
     class Meta:
@@ -45,10 +50,17 @@ class ContractSubGoal(models.Model):
 class ContractItem(models.Model):
     name = models.CharField(verbose_name="Item", max_length=128)
     description = models.CharField(verbose_name="Descrição", max_length=128)
-    total_expense = models.DecimalField(verbose_name="Despesa total", decimal_places=2)
+    total_expense = models.DecimalField(
+        verbose_name="Despesa total",
+        decimal_places=2,
+        max_digits=12,
+    )
     is_additive = models.BooleanField(verbose_name="É aditivo?", default=False)
     status = models.CharField(
-        verbose_name="Status", choices=StatusChoices, default=StatusChoices.DRAFT
+        verbose_name="Status",
+        max_length=16,
+        choices=StatusChoices,
+        default=StatusChoices.DRAFT,
     )
 
     class Meta:
