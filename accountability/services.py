3cb3c006f4146925b67995132d9a00c64a297301
Vitor Toledo <vitor.toledo@grupomateus.com>
2024-12-27 12:31:40 -0300

    add AccountabilityCSVExporter class

--- accountability/services.py
+++ accountability/services.py
@@ -2,210 +2,177 @@ import xlsxwriter
 from datetime import datetime
 
 
-# Updated upstream
-def export_accountability_csv():
-    # 1. Pegar campos no banco
-    # 2. Anaisar e transformar campos
-    # 3. Gerar arquivo xlsx
-    # 4. Retornar arquivo
+class AccountabilityCSVExporter:
+    workbook: None
 
-    prefeitura = "Várzea Pta"
+    def handle(self):
+        # Criar modelo de Fornecedor
 
-    # Create a workbook and add a worksheet.
-    workbook = xlsxwriter.Workbook("Expenses01.xlsx")
-    worksheet = workbook.add_worksheet()
+        archive = f"archive-{str(datetime.now().time())[0:8]}.xlsx"
 
-    expenses = (
-        ["Banana", 20],
-        ["Mac", 22],
-        ["Pera", 30],
-    )
+        # Cria planilha e abas.
+        self.workbook = xlsxwriter.Workbook(archive)
+        self._build_worksheets()
+        self.workbook.close()
 
-    row = 0
-    col = 0
+        return self.workbook
 
-    for item, id in expenses:
-        worksheet.write(row, col, item)
-        worksheet.write(row, col + 1, id)
-        row += 1
+    def _build_worksheets(self):
+        builders = [
+            self.__build_receipt_worksheet,
+            self.__build_expense_worksheet,
+            self.__build_application_worksheet,
+            self.__build_fr_worksheet,
+            self.__build_bank_account_worksheet,
+            self.__build_category_worksheet,
+            self.__build_expense_category_worksheet,
+            self.__build_favored_worksheet,
+            self.__build_ia_worksheet,
+            self.__build_doc_type_worksheet,
+            self.__build_pr_worksheet,
+        ]
 
-    worksheet.write(row, 0, "Total")
-    worksheet.write(row, 1, "=SUM(B1:B3)")
+        for builder in builders:
+            builder()
 
+    def __build_receipt_worksheet(self):
+        receipt_worksheet = self.workbook.add_worksheet(name="1. RECEITAS")
 
-# =======
-def _build_receipt_worksheet(workbook):
-    receipt_worksheet = workbook.add_worksheet(name="1. RECEITAS")
+        # Formatação células cabeçalho (linha 1)
+        header_format = self.workbook.add_format(
+            {
+                "bold": True,
+                # 'border':   6,
+                "align": "center",
+                "valign": "vcenter",
+                "fg_color": "#a3d7c6",
+            }
+        )
 
-    # Formatação células cabeçalho (linha 1)
-    header_format = workbook.add_format(
-        {
-            "bold": True,
-            # 'border':   6,
-            "align": "center",
-            "valign": "vcenter",
-            "fg_color": "#a3d7c6",
-        }
-    )
+        # Formatação células sub título (linha 2)
+        sub_format = self.workbook.add_format(
+            {
+                "align": "center",
+                "valign": "vcenter",
+                "fg_color": "#efe920",
+            }
+        )
 
-    # Formatação células sub título (linha 2)
-    sub_format = workbook.add_format(
-        {
-            "align": "center",
-            "valign": "vcenter",
-            "fg_color": "#efe920",
-        }
-    )
+        # Criando cabeçalho
+        header_content = (
+            ["ID DO PROJETO", "", ""],
+            ["IDENTIFICAÇÃO", "", ""],
+            ["VALOR", "", ""],
+            ["VENCIMENTO", "", ""],
+            ["COMPETÊNCIA", "", ""],
+            ["FONTE DE RECURSO", "Nome", "ID (não preencher)"],
+            ["CONTA BANCÁRIA", "Nome", "ID (não preencher)"],
+            ["NATUREZA DA RECEITA", "Nome", "ID (não preencher)"],
+            ["OBSERVAÇÕES", "", ""],
+        )
 
-    # Criando cabeçalho
-    header_content = (
-        ["ID DO PROJETO", "", ""],
-        ["IDENTIFICAÇÃO", "", ""],
-        ["VALOR", "", ""],
-        ["VENCIMENTO", "", ""],
-        ["COMPETÊNCIA", "", ""],
-        ["FONTE DE RECURSO", "Nome", "ID (não preencher)"],
-        ["CONTA BANCÁRIA", "Nome", "ID (não preencher)"],
-        ["NATUREZA DA RECEITA", "Nome", "ID (não preencher)"],
-        ["OBSERVAÇÕES", "", ""],
-    )
+        col = 0
+        for main_colum, sub1, sub2 in header_content:
+            if sub1 == "":
+                # Adiciona texto e mesclas células 1 e 2 de suas respectivas colunas
+                receipt_worksheet.merge_range(0, col, 1, col, main_colum, header_format)
 
-    col = 0
-    for main_colum, sub1, sub2 in header_content:
-        if sub1 == "":
-            # Adiciona texto e mesclas células 1 e 2 de suas respectivas colunas
-            receipt_worksheet.merge_range(0, col, 1, col, main_colum, header_format)
+            else:
+                # Adiciona sub coluna mescla colunas de acordo com o seu título
+                receipt_worksheet.merge_range(
+                    0, col, 0, col + 1, main_colum, header_format
+                )
+                receipt_worksheet.write(1, col, sub1, sub_format)
+                receipt_worksheet.write(1, col + 1, sub2, sub_format)
+                col += 1
 
-        else:
-            # Adiciona sub coluna mescla colunas de acordo com o seu título
-            receipt_worksheet.merge_range(0, col, 0, col + 1, main_colum, header_format)
-            receipt_worksheet.write(1, col, sub1, sub_format)
-            receipt_worksheet.write(1, col + 1, sub2, sub_format)
             col += 1
 
-        col += 1
+        receipt_worksheet.autofit()
+        return receipt_worksheet
 
-    receipt_worksheet.autofit()
-    return receipt_worksheet
+    def __build_expense_worksheet(self):
+        content = [""]
 
+        expense_worksheet = self.workbook.add_worksheet(name="2. DESPESAS")
+        # PREENCHER AQUI
 
-def _build_expense_worksheet(workbook):
-    content = [""]
+        return expense_worksheet
 
-    expense_worksheet = workbook.add_worksheet(name="2. DESPESAS")
-    # PREENCHER AQUI
+    def __build_application_worksheet(self):
+        content = [""]
 
-    return expense_worksheet
+        application_worksheet = self.workbook.add_worksheet(
+            name="3. APLICACOES E RESGATES"
+        )
+        # PREENCHER AQUI
 
+        return application_worksheet
 
-def _build_application_worksheet(workbook):
-    content = [""]
+    def __build_fr_worksheet(self):
+        content = [""]
 
-    application_worksheet = workbook.add_worksheet(name="3. APLICACOES E RESGATES")
-    # PREENCHER AQUI
+        fr_worksheet = self.workbook.add_worksheet(name="FR")
+        # PREENCHER AQUI
 
-    return application_worksheet
+        return fr_worksheet
 
+    def __build_bank_account_worksheet(self):
+        content = [""]
 
-def _build_fr_worksheet(workbook):
-    content = [""]
+        bank_account_worksheet = self.workbook.add_worksheet(name="CB")
+        # PREENCHER AQUI
 
-    fr_worksheet = workbook.add_worksheet(name="FR")
-    # PREENCHER AQUI
+        return bank_account_worksheet
 
-    return fr_worksheet
+    def __build_category_worksheet(self):
+        content = [""]
 
+        category_worksheet = self.workbook.add_worksheet(name="NR")
+        # PREENCHER AQUI
 
-def _build_bank_account_worksheet(workbook):
-    content = [""]
+        return category_worksheet
 
-    bank_account_worksheet = workbook.add_worksheet(name="CB")
-    # PREENCHER AQUI
+    def __build_expense_category_worksheet(self):
+        content = [""]
 
-    return bank_account_worksheet
+        expense_category_worksheet = self.workbook.add_worksheet(name="ND")
+        # PREENCHER AQUI
 
+        return expense_category_worksheet
 
-def _build_category_worksheet(workbook):
-    content = [""]
+    def __build_favored_worksheet(self):
+        content = [""]
 
-    category_worksheet = workbook.add_worksheet(name="NR")
-    # PREENCHER AQUI
+        favored_worksheet = self.workbook.add_worksheet(name="FV")
+        # PREENCHER AQUI
 
-    return category_worksheet
+        return favored_worksheet
 
+    def __build_ia_worksheet(self):
+        content = [""]
 
-def _build_expense_category_worksheet(workbook):
-    content = [""]
+        ia_worksheet = self.workbook.add_worksheet(name="IA")
+        # PREENCHER AQUI
 
-    expense_category_worksheet = workbook.add_worksheet(name="ND")
-    # PREENCHER AQUI
+        return ia_worksheet
 
-    return expense_category_worksheet
+    def __build_doc_type_worksheet(self):
+        content = [""]
 
+        doc_type_worksheet = self.workbook.add_worksheet(name="TD")
+        # PREENCHER AQUI
 
-def _build_favored_worksheet(workbook):
-    content = [""]
+        return doc_type_worksheet
 
-    favored_worksheet = workbook.add_worksheet(name="FV")
-    # PREENCHER AQUI
+    def __build_pr_worksheet(self):
+        content = [""]
 
-    return favored_worksheet
+        pr_worksheet = self.workbook.add_worksheet(name="PR")
+        # PREENCHER AQUI
 
-
-def _build_ia_worksheet(workbook):
-    content = [""]
-
-    ia_worksheet = workbook.add_worksheet(name="IA")
-    # PREENCHER AQUI
-
-    return ia_worksheet
-
-
-def _build_doc_type_worksheet(workbook):
-    content = [""]
-
-    doc_type_worksheet = workbook.add_worksheet(name="TD")
-    # PREENCHER AQUI
-
-    return doc_type_worksheet
-
-
-def _build_pr_worksheet(workbook):
-    content = [""]
-
-    pr_worksheet = workbook.add_worksheet(name="PR")
-    # PREENCHER AQUI
-
-    return pr_worksheet
-
-
-def export_accountability_csv():
-    # Criar modelo de Fornecedor
-
-    prefeitura = "Várzea Pta"
-    archive = f"archive-{str(datetime.now().time())[0:8]}.xlsx"
-
-    # Configurações de formatação básica
-
-    # Cria planilha e abas.
-    workbook = xlsxwriter.Workbook(archive)
-
-    receipt_worksheet = _build_receipt_worksheet(workbook)
-    expense_worksheet = _build_expense_worksheet(workbook)
-    application_worksheet = _build_application_worksheet(workbook)
-    fr_worksheet = _build_fr_worksheet(workbook)
-    bank_account_worksheet = _build_bank_account_worksheet(workbook)
-    category_worksheet = _build_category_worksheet(workbook)
-    expense_category_worksheet = _build_expense_category_worksheet(workbook)
-    favored_worksheet = _build_favored_worksheet(workbook)
-    ia_worksheet = _build_ia_worksheet(workbook)
-    doc_type_worksheet = _build_doc_type_worksheet(workbook)
-    pr_worksheet = _build_pr_worksheet(workbook)
-
-    workbook.close()
-
-    return workbook
+        return pr_worksheet
 
 
 if __name__ == "__main__":
-    export_accountability_csv()
+    AccountabilityCSVExporter().handle()
