a13c1b325f615f147b085820266233a75227e4df
ShRafa <rafa.ps89@gmail.com>
2025-01-08 17:54:19 -0300

    Relatorio repasse a terceiro, tentativa 2

--- reports/exporters/pass_to_third.py
+++ reports/exporters/pass_to_third.py
@@ -2,14 +2,15 @@ from datetime import datetime
 from fpdf import FPDF
 from fpdf.enums import XPos, YPos
 
-class PassToThird(FPDF):
 
+class PassToThird(FPDF):
     def header(self):
-        # Título
-        self.set_font("Helvetica", "B", 12)
+        # Cabeçalho e títulos
+        self.set_margins(20,20,20)
+        self.set_font("Helvetica", "B", 11)
         self.cell(
             0,
-            10,
+            0,
             "ANEXO RP-10 - REPASSES AO TERCEIRO SETOR",
             align="C",
             new_x=XPos.LMARGIN,
@@ -18,12 +19,14 @@ class PassToThird(FPDF):
         self.set_font("Helvetica", "", 10)
         self.cell(
             0,
-            10,
+            12,
             "DEMONSTRATIVO INTEGRAL DAS RECEITAS E DESPESAS - TERMO DE COLABORAÇÃO/FOMENTO",
             align="C",
             new_x=XPos.LMARGIN,
-            new_y=YPos.NEXT,
+            new_y=YPos.NEXT
         )
+        # Espaçamento do título pro próximo dado
+        self.set_y(self.get_y() + 5)
 
     def footer(self):
         # Rodapé
@@ -31,50 +34,59 @@ class PassToThird(FPDF):
         self.set_font("Helvetica", "I", 8)
         self.cell(0, 10, f"Page {self.page_no()}", align="C")
 
-    def add_text(self, text, bold):
-        if bold == True:
-            self.bold = "B"
-        else: 
-            self.bold = ""
-        self.set_font("Helvetica", self.bold, 9)
-        self.cell(40, 10, text, align="L")
-        self.ln(2)
+    def add_text(self, label, value="", bold=False):
+        # Adiciona texto com rótulo e valor alinhados à esquerda
+        
+        if bold:
+            self.set_font("Helvetica", "B", 8)  # Negrito
+        else:
+            self.set_font("Helvetica", "", 8)  # Normal
+
+        # Largura ajustada para alinhar o texto à esquerda
+        lenght = len(label) + 8
+        self.cell(lenght, 8, label, align="L", new_x=XPos.RIGHT, new_y=YPos.TOP)
+        
+        # Adiciona o valor (caso exista)
+        if value:
+            self.set_font("Helvetica", "", 8)
+            self.cell(0, 8, value, align="L", new_x=XPos.LMARGIN, new_y=YPos.NEXT)
 
     def add_table(self, headers, rows):
-        # Tabela do cabeçalho
+        # Adiciona colunas
         self.set_font("Helvetica", "B", 9)
         for header in headers:
             self.cell(40, 10, header, border=1, align="C")
-        self.ln()  # Move to the next line after the header row
+        self.ln()
 
-        # Linhas das colunas
+        # Adiciona linhas
         self.set_font("Helvetica", "", 9)
         for row in rows:
             for col in row:
                 self.cell(40, 10, str(col), border=1, align="C")
-            self.ln()  # Move to the next line after each row
+            self.ln()
 
 
-# Create PDF object
+# Gera PDF
 pdf = PassToThird()
 pdf.add_page()
 
 # Dados da Organização
-pdf.add_text("Órgão Público", True)
-pdf.add_text("Prefeitura Municipal de Várzea Paulista", False)
-pdf.add_text("Organização da Sociedade Civil", True)
-pdf.add_text("Associação Comunidade Varzina - Eco & Vida (Meio Ambiente)", False)
-pdf.add_text("CNPJ", True)
-pdf.add_text("02.834.119/0001-95", False)
-pdf.add_text("Endereço e CEP", True)
-pdf.add_text("Rua Feres Sada 82 - Loteamento Parque Empresarial São Luís", False)
-pdf.add_text("Responsáveis pela OSC", True)
-# "Nome, Papel, CPF")
+pdf.add_text("Órgão Público:", "Prefeitura Municipal de Várzea Paulista", bold=True)
+# pdf.add_text("Organização da Sociedade Civil", bold=True)
+# pdf.add_text("Associação Comunidade Varzina - Eco & Vida (Meio Ambiente)")
+# pdf.add_text("CNPJ", bold=True)
+# pdf.add_text("02.834.119/0001-95")
+# pdf.add_text("Endereço e CEP", bold=True)
+# pdf.add_text("Rua Feres Sada 82 - Loteamento Parque Empresarial São Luís")
+# pdf.add_text("Responsáveis pela OSC", bold=True)
 
 # Dados da parceria
-pdf.add_text("Objeto da Parceria", "Executar a coleta de recicláveis no município de Várzea Paulista - SP")
-pdf.add_text("Exercício", "01/11/2024 a 30/11/2024")
-pdf.add_text("Origem dos Recursos (1)", "Consolidado de todas as fontes")
+# pdf.add_text("Objeto da Parceria", bold=True)
+# pdf.add_text("Executar a coleta de recicláveis no município de Várzea Paulista - SP")
+# pdf.add_text("Exercício", bold=True)
+# pdf.add_text("01/11/2024 a 30/11/2024")
+# pdf.add_text("Origem dos Recursos (1)", bold=True)
+# pdf.add_text("Consolidado de todas as fontes")
 
 # Tabela "DOCUMENTO"
 headers = ["DOCUMENTO", "DATA", "VIGÊNCIA", "VALOR - R$"]
@@ -85,7 +97,7 @@ rows = [
 pdf.add_table(headers, rows)
 
 # Tabela "DEMONSTRATIVO DOS RECURSOS"
-pdf.add_text("DEMONSTRATIVO DOS RECURSOS DISPONÍVEIS NO EXERCÍCIO", "B")
+pdf.add_text("DEMONSTRATIVO DOS RECURSOS DISPONÍVEIS NO EXERCÍCIO", bold=True)
 headers = [
     "DATA PREVISTA PARA O REPASSE (2)",
     "VALORES PREVISTOS (R$)",
@@ -97,20 +109,20 @@ rows = [["10/2024", "64.682,75", "05/11/2024", "552766000230001", "64.682,75"]]
 pdf.add_table(headers, rows)
 
 # Resumo
-pdf.add_text("(A) SALDO DO EXERCÍCIO ANTERIOR", False)
-pdf.add_text("R$ 42.554,87", False)
-pdf.add_text("(B) REPASSES PÚBLICOS NO EXERCÍCIO", False)
-pdf.add_text("R$ 64.682,75", False)
-pdf.add_text("(C) RECEITAS COM APLICAÇÕES FINANCEIRAS DOS REPASSES PÚBLICOS", False)
-pdf.add_text("R$ 0,00", False)
-pdf.add_text("(D) OUTRAS RECEITAS DECORRENTES DA EXECUÇÃO DO AJUSTE (3)", False)
-pdf.add_text("R$ 0,00", False)
-pdf.add_text("(E) TOTAL DE RECURSOS PÚBLICOS (A + B + C + D)", False)
-pdf.add_text("R$ 107.237,62", False)
-pdf.add_text("(F) RECURSOS PRÓPRIOS DA ENTIDADE PARCEIRA", False)
-pdf.add_text("R$ 0,00", False)
-pdf.add_text("(G) TOTAL DE RECURSOS DISPONÍVEIS NO EXERCÍCIO (E + F)", False)
-pdf.add_text("R$ 107.237,62", False)
+# pdf.add_text("(A) SALDO DO EXERCÍCIO ANTERIOR", bold=True)
+# pdf.add_text("R$ 42.554,87")
+# pdf.add_text("(B) REPASSES PÚBLICOS NO EXERCÍCIO", bold=True)
+# pdf.add_text("R$ 64.682,75")
+# pdf.add_text("(C) RECEITAS COM APLICAÇÕES FINANCEIRAS DOS REPASSES PÚBLICOS", bold=True)
+# pdf.add_text("R$ 0,00")
+# pdf.add_text("(D) OUTRAS RECEITAS DECORRENTES DA EXECUÇÃO DO AJUSTE (3)", bold=True)
+# pdf.add_text("R$ 0,00")
+# pdf.add_text("(E) TOTAL DE RECURSOS PÚBLICOS (A + B + C + D)", bold=True)
+# pdf.add_text("R$ 107.237,62")
+# pdf.add_text("(F) RECURSOS PRÓPRIOS DA ENTIDADE PARCEIRA", bold=True)
+# pdf.add_text("R$ 0,00")
+# pdf.add_text("(G) TOTAL DE RECURSOS DISPONÍVEIS NO EXERCÍCIO (E + F)", bold=True)
+# pdf.add_text("R$ 107.237,62")
 
-# Save the PDF
+# Salva PDF
 pdf.output(f"rp10-{str(datetime.now().time())[0:8]}.pdf")
