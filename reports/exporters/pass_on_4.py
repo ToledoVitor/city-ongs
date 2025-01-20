753fed7986799f44d66445738fa5281b5ebe2666
ShRafa <rafa.ps89@gmail.com>
2025-01-20 19:13:19 -0300

    Até que enfim rp4

--- reports/exporters/pass_on_4.py
+++ reports/exporters/pass_on_4.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 
 from fpdf import FPDF, XPos, YPos
+from fpdf.fonts import FontFace
 
 
 @dataclass
@@ -26,10 +27,11 @@ class PassOn4PDFExporter:
         self._draw_header()
         self._draw_informations()
         self._draw_manager_table()
-        # self._draw_documents_table()
-        # self._draw_resources_table()
-        # self._draw_resume_table()
-        # self._draw_table_footer()
+        self._draw_partner_table()
+        self._draw_collaborator_table()
+        self._draw_promotion_table()
+        self._draw_agreement_table()
+        self._draw_concession_table()
         self._draw_footer()
 
         return self.pdf
@@ -82,209 +84,336 @@ class PassOn4PDFExporter:
         self.__set_helvetica_font(font_size=7, bold=True)
         self.pdf.ln()
         headers = [
-            "Contrato de Gestão Nº",
-            "Beneficiário",
-            "CNPJ",
-            "Endereço",
-            "Data",
-            "Vigência até",
-            "Valor Global do Ajuste",
-            "Objeto",
-            "Fonte",
-            "Valor Repassado no Exercício",
+            ("Contrato de Gestão Nº"),
+            ("Beneficiário"),
+            ("CNPJ"),
+            ("Endereço"),
+            ("Data"),
+            ("Vigência até"),
+            ("Valor Global do Ajuste"),
+            ("Objeto"),
+            ("Fonte"),
+            ("Valor Repassado no Exercício"),
         ]
         table_data = []
-        total_line = ["Total:" "R$ 0,00"]
+        total_line = [
+            ("Total:"),
+            ("R$ 0,00"),
+        ]
 
-        col_widths = [35, 25, 10, 15, 10, 15, 20, 10, 10, 50]
-        self.__set_helvetica_font(font_size=8, bold=True)
+        col_widths = [15, 20, 10, 15, 15, 15, 20, 30, 20, 20]
+        font = FontFace("Helvetica", "B", size_pt=6)
 
-        for col_index, header in enumerate(headers):
-            self.pdf.cell(
-                col_widths[col_index],
-                h=self.default_cell_height,
-                text=header,
-                border=1,
-                align="L",
-            )
+        with self.pdf.table(
+            headings_style=font,
+            line_height=6,
+            align="L",
+            col_widths=col_widths,
+        ) as table:
+            header = table.row()
+            for datum_index, datum_text in enumerate(headers):
+                header.cell(datum_text)
+            if table_data != []:
+                data = table.row()
+                for datum_index, datum_text in enumerate(table_data):
+                    data.cell(datum_text)
+
+        col_widths = [178, 22]
+        font = FontFace("Helvetica", "B", size_pt=6)
+        with self.pdf.table(
+            headings_style=font,
+            line_height=6,
+            align="L",
+            col_widths=col_widths,
+        ) as table:
+            total = table.row()
+            for datum_index, datum_text in enumerate(total_line):
+                total.cell(datum_text)
+
+        self.pdf.ln(10)
+
+    def _draw_partner_table(self):
+        self.__set_helvetica_font(font_size=7, bold=True)
         self.pdf.ln()
-
-        self.__set_helvetica_font()
-        for row in table_data:
-            for col_index, col_text in enumerate(row):
-                self.pdf.cell(
-                    col_widths[col_index],
-                    h=self.default_cell_height,
-                    text=col_text,
-                    border=1,
-                    align="L",
-                )
-            self.pdf.ln()
-
-        self.__set_helvetica_font(font_size=8, bold=True)
-        col_widths = [150, 50]
-        for col_index, total_line in enumerate(total_line):
-            self.pdf.cell(
-                col_widths[col_index],
-                h=self.default_cell_height,
-                text=header,
-                border=1,
-                align="L",
-            )
-        self.pdf.ln()
-
-    def _draw_resources_table(self):
-        self.pdf.ln(6)
-
-        self.__set_helvetica_font(font_size=8, bold=True)
-        self.pdf.cell(
-            190,
-            h=self.default_cell_height,
-            text="DEMONSTRATIVO DOS RECURSOS DISPONÍVEIS NO EXERCÍCIO",
-            border=1,
-            align="C",
-        )
-        self.pdf.ln(self.default_cell_height)
-
         headers = [
-            "DATA PREVISTA PARA O REPASSE (2)",
-            "VALORES PREVISTOS (R$)",
-            "DATA DO REPASSE",
-            "NÚMERO DO DOCUMENTO DE CRÉDITO",
-            "VALORES REPASSADOS (R$)",
+            ("Termo de Parceria Nº"),
+            ("Beneficiário"),
+            ("CNPJ"),
+            ("Endereço"),
+            ("Data"),
+            ("Vigência até"),
+            ("Valor Global do Ajuste"),
+            ("Objeto"),
+            ("Fonte"),
+            ("Valor Repassado no Exercício"),
+        ]
+        table_data = []
+        total_line = [
+            ("Total:"),
+            ("R$ 0,00"),
+        ]
+
+        col_widths = [15, 20, 10, 15, 15, 15, 20, 30, 20, 20]
+        font = FontFace("Helvetica", "B", size_pt=6)
+
+        with self.pdf.table(
+            headings_style=font,
+            line_height=6,
+            align="L",
+            col_widths=col_widths,
+        ) as table:
+            header = table.row()
+            for datum_index, datum_text in enumerate(headers):
+                header.cell(datum_text)
+            if table_data != []:
+                data = table.row()
+                for datum_index, datum_text in enumerate(table_data):
+                    data.cell(datum_text)
+
+        col_widths = [178, 22]
+        font = FontFace("Helvetica", "B", size_pt=6)
+        with self.pdf.table(
+            headings_style=font,
+            line_height=6,
+            align="L",
+            col_widths=col_widths,
+        ) as table:
+            total = table.row()
+            for datum_index, datum_text in enumerate(total_line):
+                total.cell(datum_text)
+
+        self.pdf.ln(10)
+
+    def _draw_collaborator_table(self):
+        self.__set_helvetica_font(font_size=7, bold=True)
+        self.pdf.ln()
+        headers = [
+            ("Termo de Colaboração Nº"),
+            ("Beneficiário"),
+            ("CNPJ"),
+            ("Endereço"),
+            ("Data"),
+            ("Vigência até"),
+            ("Valor Global do Ajuste"),
+            ("Objeto"),
+            ("Fonte"),
+            ("Valor Repassado no Exercício"),
         ]
         table_data = [
-            [
-                "10/2024",
-                "64.682,75",
-                "05/11/2024",
-                "552766000230001",
-                "64.682,75",
-            ],
+            ("10/2023"),
+            ("Associação Comunidade Varzina - Eco & Vida (Meio Ambiente)"),
+            ("02.834.119/0001-95"),
+            (
+                "Rua Feres Sada, 82 - Loteamento Parque Empresarial São Luiz, Várzea Paulista - null, Brasil"
+            ),
+            ("26/09/2023"),
+            ("26/09/2023"),
+            ("R$ 1.538.185,32"),
+            (
+                "Executar a coleta de recicláveis no município de Várzea Paulista - SP, em acordo com a Política Nacional de Resíduos Sólidos (PNRS)"
+            ),
+            ("Consolidado de todas as fontes"),
+            ("R$ 0,00"),
         ]
-        col_widths = [40, 35, 25, 50, 40]
+        total_line = [
+            ("Total:"),
+            ("R$ 0,00"),
+        ]
 
-        for col_index, header in enumerate(headers):
-            x = self.pdf.get_x()
-            y = self.pdf.get_y()
-            self.pdf.multi_cell(col_widths[col_index], 4, header, border=1, align="C")
-            self.pdf.set_xy(x + col_widths[col_index], y)
-        self.pdf.ln(8)
+        col_widths = [15, 20, 10, 15, 15, 15, 20, 30, 20, 20]
+        font = FontFace("Helvetica", "B", size_pt=6)
 
-        self.__set_helvetica_font()
-        for row in table_data:
-            for col_index, col_text in enumerate(row):
-                if col_index == 4:
-                    self.pdf.cell(
-                        col_widths[col_index],
-                        h=self.default_cell_height,
-                        text=col_text,
-                        border=1,
-                        align="R",
-                    )
-                else:
-                    self.pdf.cell(
-                        col_widths[col_index],
-                        h=self.default_cell_height,
-                        text=col_text,
-                        border=1,
-                        align="C",
-                    )
-            self.pdf.ln()
+        with self.pdf.table(
+            headings_style=font,
+            line_height=6,
+            align="L",
+            col_widths=col_widths,
+        ) as table:
+            header = table.row()
+            for datum_index, datum_text in enumerate(headers):
+                header.cell(datum_text)
+            if table_data != []:
+                self.pdf.set_font("Helvetica", "", 5)
+                data = table.row()
+                for datum_index, datum_text in enumerate(table_data):
+                    data.cell(datum_text)
 
-        # Linha cinza
-        self.pdf.cell(
-            190, h=self.default_cell_height, text="", border=1, align="C", fill=True
-        )
+        col_widths = [178, 22]
+        font = FontFace("Helvetica", "B", size_pt=6)
+        with self.pdf.table(
+            headings_style=font,
+            line_height=6,
+            align="L",
+            col_widths=col_widths,
+        ) as table:
+            total = table.row()
+            for datum_index, datum_text in enumerate(total_line):
+                total.cell(datum_text)
+
+        self.pdf.ln(10)
+
+    def _draw_promotion_table(self):
+        self.__set_helvetica_font(font_size=7, bold=True)
+        self.pdf.ln(3)
+        headers = [
+            ("Contrato de Fomento Nº"),
+            ("Beneficiário"),
+            ("CNPJ"),
+            ("Endereço"),
+            ("Data"),
+            ("Vigência até"),
+            ("Valor Global do Ajuste"),
+            ("Objeto"),
+            ("Fonte"),
+            ("Valor Repassado no Exercício"),
+        ]
+        table_data = []
+        total_line = [
+            ("Total:"),
+            ("R$ 0,00"),
+        ]
+
+        col_widths = [15, 20, 10, 15, 15, 15, 20, 30, 20, 20]
+        font = FontFace("Helvetica", "B", size_pt=6)
+
+        with self.pdf.table(
+            headings_style=font,
+            line_height=6,
+            align="L",
+            col_widths=col_widths,
+        ) as table:
+            header = table.row()
+            for datum_index, datum_text in enumerate(headers):
+                header.cell(datum_text)
+            if table_data != []:
+                data = table.row()
+                for datum_index, datum_text in enumerate(table_data):
+                    data.cell(datum_text)
+
+        col_widths = [178, 22]
+        font = FontFace("Helvetica", "B", size_pt=6)
+        with self.pdf.table(
+            headings_style=font,
+            line_height=6,
+            align="L",
+            col_widths=col_widths,
+        ) as table:
+            total = table.row()
+            for datum_index, datum_text in enumerate(total_line):
+                total.cell(datum_text)
+
+        self.pdf.ln(10)
+
+    def _draw_agreement_table(self):
+        self.__set_helvetica_font(font_size=7, bold=True)
+        self.pdf.ln()
+        headers = [
+            ("Convênio Nº"),
+            ("Beneficiário"),
+            ("CNPJ"),
+            ("Endereço"),
+            ("Data"),
+            ("Vigência até"),
+            ("Valor Global do Ajuste"),
+            ("Objeto"),
+            ("Fonte"),
+            ("Valor Repassado no Exercício"),
+        ]
+        table_data = []
+        total_line = [
+            ("Total:"),
+            ("R$ 0,00"),
+        ]
+
+        col_widths = [15, 20, 10, 15, 15, 15, 20, 30, 20, 20]
+        font = FontFace("Helvetica", "B", size_pt=6)
+
+        with self.pdf.table(
+            headings_style=font,
+            line_height=6,
+            align="L",
+            col_widths=col_widths,
+        ) as table:
+            header = table.row()
+            for datum_index, datum_text in enumerate(headers):
+                header.cell(datum_text)
+            if table_data != []:
+                data = table.row()
+                for datum_index, datum_text in enumerate(table_data):
+                    data.cell(datum_text)
+
+        col_widths = [178, 22]
+        font = FontFace("Helvetica", "B", size_pt=6)
+        with self.pdf.table(
+            headings_style=font,
+            line_height=6,
+            align="L",
+            col_widths=col_widths,
+        ) as table:
+            total = table.row()
+            for datum_index, datum_text in enumerate(total_line):
+                total.cell(datum_text)
+
         self.pdf.ln(self.default_cell_height)
 
-    def _draw_resume_table(self):
-        extern_revenue_data = [
-            ["(A) SALDO DO EXERCÍCIO ANTERIOR", "", "R$ 42.554,87"],
-            ["(B) REPASSES PÚBLICOS NO EXERCÍCIO", "", "R$ 64.682,75"],
-            [
-                "(C) RECEITAS COM APLICAÇÕES FINANCEIRAS DOS REPASSES PÚBLICOS",
-                "",
-                "R$ 0,00",
-            ],
-            [
-                "(D) OUTRAS RECEITAS DECORRENTES DA EXECUÇÃO DO AJUSTE (3)",
-                "",
-                "R$ 0,00",
-            ],
-            ["(E) TOTAL DE RECURSOS PÚBLICOS (A + B + C + D)", "", "R$ 107.237,62"],
-            ["", "", ""],
+    def _draw_concession_table(self):
+        self.__set_helvetica_font(font_size=8, bold=True)
+        self.pdf.cell(
+            text="II - AUXÍLIOS, SUBVENÇÕES E/OU CONTRIBUIÇÕES PAGOS:",
+            markdown=True,
+            h=self.default_cell_height,
+        )
+        self.pdf.ln(2)
+        self.__set_helvetica_font(font_size=7, bold=True)
+        self.pdf.ln()
+        headers = [
+            ("Tipo da Concessão"),
+            ("Beneficiário"),
+            ("CNPJ"),
+            ("Endereço"),
+            ("Data"),
+            ("Vigência até"),
+            ("Valor Global do Ajuste"),
+            ("Objeto"),
+            ("Fonte"),
+            ("Valor Repassado no Exercício"),
         ]
-        intern_revenue_data = [
-            ["(F) RECURSOS PRÓPRIOS DA ENTIDADE PARCEIRA", "", "R$ 0,00"],
-            [
-                "(G) TOTAL DE RECURSOS DISPONÍVEIS NO EXERCÍCIO (E + F)",
-                "",
-                "R$ 107.237,62",
-            ],
+        table_data = []
+        total_line = [
+            ("Total:"),
+            ("R$ 0,00"),
         ]
 
-        self.__set_helvetica_font()
-        col_widths = [100, 50, 40]
+        col_widths = [15, 20, 10, 15, 15, 15, 20, 30, 20, 20]
+        font = FontFace("Helvetica", "B", size_pt=6)
 
-        for row in extern_revenue_data:
-            for col_index, col_text in enumerate(row):
-                if col_index == 2:
-                    self.pdf.cell(
-                        col_widths[col_index],
-                        h=self.default_cell_height,
-                        text=col_text,
-                        border=1,
-                        align="R",
-                    )
-                else:
-                    self.pdf.cell(
-                        col_widths[col_index],
-                        h=self.default_cell_height,
-                        text=col_text,
-                        border=1,
-                        align="L",
-                    )
-            self.pdf.ln()
+        with self.pdf.table(
+            headings_style=font,
+            line_height=6,
+            align="L",
+            col_widths=col_widths,
+        ) as table:
+            header = table.row()
+            for datum_index, datum_text in enumerate(headers):
+                header.cell(datum_text)
+            if table_data != []:
+                data = table.row()
+                for datum_index, datum_text in enumerate(table_data):
+                    data.cell(datum_text)
 
-        for row_index, row in enumerate(intern_revenue_data):
-            for col_index, col_text in enumerate(row):
-                if col_index == 2:
-                    self.pdf.cell(
-                        col_widths[col_index],
-                        h=self.default_cell_height,
-                        text=col_text,
-                        border=1,
-                        align="R",
-                    )
-                else:
-                    self.pdf.cell(
-                        col_widths[col_index],
-                        h=self.default_cell_height,
-                        text=col_text,
-                        border=1,
-                        align="L",
-                    )
-            if row_index != len(intern_revenue_data) - 1:
-                self.pdf.ln()
+        col_widths = [178, 22]
+        font = FontFace("Helvetica", "B", size_pt=6)
+        with self.pdf.table(
+            headings_style=font,
+            line_height=6,
+            align="L",
+            col_widths=col_widths,
+        ) as table:
+            total = table.row()
+            for datum_index, datum_text in enumerate(total_line):
+                total.cell(datum_text)
 
-    def _draw_table_footer(self):
         self.pdf.ln(self.default_cell_height)
-        self.__set_helvetica_font()
-        self.pdf.cell(
-            text="(1) Verba: Federal, Estadual ou Municipal, devendo ser elaborado um anexo para cada fonte de recurso.",
-            h=self.default_cell_height,
-        )
-        self.pdf.ln(3)
-        self.pdf.cell(
-            text="(2) Incluir valores previstos no exercício anterior e repassados neste exercício.",
-            h=self.default_cell_height,
-        )
-        self.pdf.ln(3)
-        self.pdf.cell(
-            text="(3) Receitas com estacionamento, aluguéis, entre outras.",
-            h=self.default_cell_height,
-        )
 
     def _draw_footer(self):
         # Rodapé
