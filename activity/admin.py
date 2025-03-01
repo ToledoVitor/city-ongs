2b85108f9b0679d840794435e407d9febc5f5eae
ToledoVitor <pr.vitortoledo@gmail.com>
2025-02-28 22:54:34 -0300

    anchor link and notification admin

--- activity/admin.py
+++ activity/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from activity.models import ActivityLog
+from activity.models import ActivityLog, Notification
+
 
 admin.site.register(ActivityLog)
+admin.site.register(Notification)
