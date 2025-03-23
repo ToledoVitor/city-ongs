473ba72ef7d5d6680d211a7e1c44b30f9bc49cfb
ToledoVitor <pr.vitortoledo@gmail.com>
2025-03-23 12:21:20 -0300

    admin improvals

--- accountability/admin.py
+++ accountability/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
+from utils.admin import BaseModelAdmin
 from accountability.models import (
     Accountability,
     AccountabilityFile,
@@ -12,12 +14,166 @@ from accountability.models import (
     RevenueFile,
 )
 
-admin.site.register(Accountability)
-admin.site.register(AccountabilityFile)
-admin.site.register(ResourceSource)
-admin.site.register(Expense)
-admin.site.register(ExpenseAnalysis)
-admin.site.register(Revenue)
-admin.site.register(ExpenseFile)
-admin.site.register(RevenueFile)
-admin.site.register(Favored)
+
+class AccountabilityFileInline(admin.TabularInline):
+    model = AccountabilityFile
+    extra = 0
+    fields = ("name", "type")
+
+
+@admin.register(Accountability)
+class AccountabilityAdmin(BaseModelAdmin):
+    list_display = (
+        "organization",
+        "contract",
+        "month",
+        "year",
+        "status",
+        "created_at",
+    )
+    list_filter = (
+        "organization",
+        "status",
+        "contract",
+        "month",
+        "year",
+    )
+    search_fields = ("contract__name", "month", "year")
+    inlines = [AccountabilityFileInline]
+    fieldsets = (
+        (_("Informações Básicas"), {
+            "fields": (
+                "organization",
+                "contract",
+                "status",
+                "description",
+            )
+        }),
+        (_("Datas"), {
+            "fields": ("start_date", "end_date", "created_at", "updated_at")
+        }),
+    )
+
+
+class ExpenseAnalysisInline(admin.TabularInline):
+    model = ExpenseAnalysis
+    extra = 0
+    fields = ("status", "description")
+
+
+class ExpenseFileInline(admin.TabularInline):
+    model = ExpenseFile
+    extra = 0
+    fields = ("name",)
+
+
+@admin.register(Expense)
+class ExpenseAdmin(BaseModelAdmin):
+    list_display = (
+        "organization",
+        "accountability",
+        "favored",
+        "value",
+        "competency",
+        "status",
+        "created_at",
+    )
+    list_filter = (
+        "organization",
+        "accountability",
+        "status",
+        "competency",
+        "paid",
+        "conciled",
+    )
+    search_fields = ("identification", "favored__name", "accountability__contract__name")
+    inlines = [ExpenseFileInline]
+    fieldsets = (
+        (_("Informações Básicas"), {
+            "fields": (
+                "organization",
+                "accountability",
+                "favored",
+                "value",
+                "date",
+            )
+        }),
+        (_("Detalhes"), {
+            "fields": ("description", "created_at", "updated_at")
+        }),
+    )
+
+
+class RevenueFileInline(admin.TabularInline):
+    model = RevenueFile
+    extra = 0
+    fields = ("name",)
+
+
+@admin.register(Revenue)
+class RevenueAdmin(BaseModelAdmin):
+    list_display = (
+        "organization",
+        "accountability",
+        "source",
+        "value",
+        "competency",
+        "receive_date",
+        "created_at",
+    )
+    list_filter = (
+        "organization",
+        "accountability",
+        "source",
+        "competency",
+        "receive_date",
+    )
+    search_fields = (
+        "description", "accountability__contract__number",
+        "source__name"
+    )
+    inlines = [RevenueFileInline]
+    fieldsets = (
+        (_("Informações Básicas"), {
+            "fields": (
+                "organization",
+                "accountability",
+                "source",
+                "value",
+                "date",
+            )
+        }),
+        (_("Detalhes"), {
+            "fields": ("description", "created_at", "updated_at")
+        }),
+    )
+
+
+@admin.register(Favored)
+class FavoredAdmin(BaseModelAdmin):
+    list_display = (
+        "organization",
+        "name",
+        "document",
+        "created_at",
+    )
+    list_filter = ("organization",)
+    search_fields = ("name", "document")
+
+
+@admin.register(ResourceSource)
+class ResourceSourceAdmin(BaseModelAdmin):
+    list_display = (
+        "organization",
+        "name",
+        "document",
+        "origin",
+        "category",
+        "created_at",
+    )
+    list_filter = (
+        "organization",
+        "origin",
+        "category",
+    )
+    search_fields = ("name", "document", "contract_number")
--- contracts/admin.py
+++ contracts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
+from utils.admin import BaseModelAdmin
 from contracts.models import (
     Company,
     Contract,
@@ -15,15 +17,166 @@ from contracts.models import (
     ContractStep,
 )
 
-admin.site.register(Company)
-admin.site.register(Contract)
-admin.site.register(ContractAddendum)
-admin.site.register(ContractGoal)
-admin.site.register(ContractStep)
-admin.site.register(ContractItem)
-admin.site.register(ContractExecution)
-admin.site.register(ContractExecutionActivity)
-admin.site.register(ContractExecutionFile)
-admin.site.register(ContractGoalReview)
-admin.site.register(ContractItemReview)
-admin.site.register(ContractItemNewValueRequest)
+
+class ContractAddendumInline(admin.TabularInline):
+    model = ContractAddendum
+    extra = 0
+    fields = ("number", "status", "description")
+
+
+class ContractGoalInline(admin.TabularInline):
+    model = ContractGoal
+    extra = 0
+    fields = ("description", "status")
+
+
+class ContractStepInline(admin.TabularInline):
+    model = ContractStep
+    extra = 0
+    readonly_fields = ("created_at",)
+    fields = (
+        "name",
+        "status",
+        "description",
+        "created_at",
+    )
+
+
+class ContractItemInline(admin.TabularInline):
+    model = ContractItem
+    extra = 0
+    fields = ("description", "value")
+
+
+class ContractExecutionInline(admin.TabularInline):
+    model = ContractExecution
+    extra = 0
+    fields = ("status", "start_date", "end_date", "description")
+
+
+class ContractItemNewValueRequestInline(admin.TabularInline):
+    model = ContractItemNewValueRequest
+    fk_name = "raise_item"
+    extra = 0
+    readonly_fields = ("created_at",)
+    fields = (
+        "raise_item",
+        "downgrade_item",
+        "value",
+        "status",
+        "created_at",
+    )
+
+
+@admin.register(Company)
+class CompanyAdmin(BaseModelAdmin):
+    list_display = ("organization", "name", "cnpj", "created_at")
+    list_filter = ("organization",)
+    search_fields = ("name", "cnpj")
+
+
+@admin.register(Contract)
+class ContractAdmin(BaseModelAdmin):
+    list_display = (
+        "organization",
+        "name",
+        "status",
+        "created_at",
+    )
+    list_filter = (
+        "organization",
+        "status",
+        "created_at",
+    )
+    search_fields = ("name", "description")
+    inlines = [
+        ContractGoalInline,
+        ContractItemInline,
+    ]
+    fieldsets = (
+        (_("Informações Básicas"), {
+            "fields": (
+                "organization",
+                "name",
+                "status",
+                "description",
+            )
+        }),
+        (_("Datas"), {
+            "fields": (
+                "created_at",
+                "updated_at",
+            )
+        }),
+        (_("Valores"), {
+            "fields": ("total_value", "monthly_value")
+        }),
+    )
+
+
+class ContractExecutionActivityInline(admin.TabularInline):
+    model = ContractExecutionActivity
+    extra = 0
+    fields = ("type", "status", "description")
+
+
+class ContractExecutionFileInline(admin.TabularInline):
+    model = ContractExecutionFile
+    extra = 0
+    fields = ("name", "type")
+
+
+@admin.register(ContractExecution)
+class ContractExecutionAdmin(BaseModelAdmin):
+    list_display = (
+        "organization",
+        "contract",
+        "status",
+        "created_at",
+    )
+    list_filter = (
+        "organization",
+        "status",
+        "contract",
+        "created_at",
+    )
+    search_fields = ("contract__name", "description")
+    inlines = [ContractExecutionActivityInline, ContractExecutionFileInline]
+
+
+class ContractGoalReviewInline(admin.TabularInline):
+    model = ContractGoalReview
+    extra = 0
+    fields = ("status", "description")
+
+
+@admin.register(ContractGoal)
+class ContractGoalAdmin(BaseModelAdmin):
+    list_display = (
+        "organization",
+        "contract",
+        "name",
+        "created_at",
+    )
+    list_filter = ("organization", "status", "contract")
+    search_fields = ("name", "contract__name")
+    inlines = [ContractGoalReviewInline]
+
+
+class ContractItemReviewInline(admin.TabularInline):
+    model = ContractItemReview
+    extra = 0
+    fields = ("status", "description")
+
+
+@admin.register(ContractItem)
+class ContractItemAdmin(BaseModelAdmin):
+    list_display = (
+        "organization",
+        "contract",
+        "name",
+        "created_at",
+    )
+    list_filter = ("organization", "contract")
+    search_fields = ("name", "contract__name")
+    inlines = [ContractItemReviewInline, ContractItemNewValueRequestInline]
